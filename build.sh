508ed692183d05262bea765cfbe82dbb6ab10a44
Frankslu <1653954675@qq.com>
2025-11-06 17:09:39 +0800

    move XiangShan build dir to an ignored dir

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ cmake -D COSIM=1 ..
 make -j
 cd $NOOP_HOME
 
-[ -e build ] && mv build build_$(date +%Y%m%d_%H_%M_%d)
+[ -e build ] && mv build build.$(date +%Y%m%d_%H_%M_%d).tmp
 make clean
 make -C $NOOP_HOME emu -j$(( $(nproc) / 2 )) SIM_ARGS="" EMU_THREADS=$threads WITH_DRAMSIM3=1 EMU_TRACE=1 CONFIG=KunminghuV2Config 2>&1 | tee $pwd_tmp/.build.log
 
