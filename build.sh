566e025d7dc394e5d3f649244d1dfa8051f26a75
Frankslu <1653954675@qq.com>
2025-11-12 18:55:36 +0800

    mv logic to function

--- build.sh
+++ build.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-########## env ##########
-DIVLINE=$(perl -E "print '=' x 20")
-set -e
-pwd_tmp=$(pwd)
-function set_env(){
-  cd ../
-  . ./env.sh
-  cd XiangShan
-}
-set_env
+function func() {(
+  set -e
+	pwd_tmp=$(pwd)
+	function set_env(){
+	  cd ../
+	  . ./env.sh
+	  cd XiangShan
+	}
+	set_env
 
-########## param: YOU SHOULD CONFIRM ##########
-threads=16 # notice threads should keep same between build.sh and run.sh
+	########## param: YOU SHOULD CONFIRM ##########
+	threads=16 # notice threads should keep same between build.sh and run.sh
 
-########## make ##########
-echo "========== make start at $(date) =========="
+	########## make ##########
+	echo "========== make start at $(date) =========="
 
-cd $DRAMSIM3_HOME
-git checkout cosim-kmh
-mkdir build -p && cd build
-cmake -D COSIM=1 ..
-make -j
-cd $NOOP_HOME
+	cd $DRAMSIM3_HOME
+	git checkout cosim-kmh
+	mkdir build -p && cd build
+	cmake -D COSIM=1 ..
+	make -j
+	cd $NOOP_HOME
 
-[ -e build ] && mv build build.$(date +%Y%m%d_%H_%M_%d).tmp
-make clean
-make -C $NOOP_HOME emu -j$(( $(nproc) / 2 )) SIM_ARGS="" EMU_THREADS=$threads WITH_DRAMSIM3=1 EMU_TRACE=1 CONFIG=KunminghuV2Config 2>&1 | tee $pwd_tmp/.build.log
+  [ -e build ] && mv build build.$(date +%Y%m%d_%H_%M_%d).tmp
+  make clean
+  make -C $NOOP_HOME emu -j$(( $(nproc) / 2 )) SIM_ARGS="" EMU_THREADS=$threads WITH_DRAMSIM3=1 EMU_TRACE=1 CONFIG=KunminghuV2Config 2>&1 | tee $pwd_tmp/.build.log
 
-echo "========== make end at $(date) =========="
-
+  echo "========== make end at $(date) =========="
+)}
+func
 python3 /nfs/home/share/liyanqin/scripts/ShareAutoEmailAlert.py -r $? --content "XiangShan build finish"
--- run.sh
+++ run.sh
@@ -1,70 +1,71 @@
 #!/bin/bash
 ########## env ##########
-DIVLINE=$(perl -E "print '=' x 20")
-pwd_tmp=$(pwd)
-set -e
-function set_env(){
-  cd ../
-  . ./env.sh
-  cd XiangShan
+function func() {(
+	pwd_tmp=$(pwd)
+	set -e
+	function set_env(){
+	  cd ../
+	  . ./env.sh
+	  cd XiangShan
 
-  PERF_HOME=/nfs/home/share/liyanqin/env-scripts/perf
+	  PERF_HOME=/nfs/home/share/liyanqin/env-scripts/perf
 
-  gcc12O3_1=/nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o3_20m_gcc12-fpcontr-off
-  cpt_path_1=$gcc12O3_1/take_cpt
-  cover1_path_1=$gcc12O3_1/json/o3_spec_fp_int-with-jemXalanc.json
-  cover3_path_1=$PERF_HOME/json/gcc12o3-fpcontr-off-0.3.json
-  cover8_path_1=$PERF_HOME/json/gcc12o3-fpcontr-off-0.8.json
+	  gcc12O3_1=/nfs-nvme/home/share/checkpoints_profiles/spec06_rv64gcb_o3_20m_gcc12-fpcontr-off
+	  cpt_path_1=$gcc12O3_1/take_cpt
+	  cover1_path_1=$gcc12O3_1/json/o3_spec_fp_int-with-jemXalanc.json
+	  cover3_path_1=$PERF_HOME/json/gcc12o3-fpcontr-off-0.3.json
+	  cover8_path_1=$PERF_HOME/json/gcc12o3-fpcontr-off-0.8.json
 
-  gcc12O3_2=/nfs/home/share/liyanqin/spec06_rv64gcb_O3_20m_gcc12.2.0-intFpcOff-jeMalloc
-  cpt_path_2=$gcc12O3_2/checkpoint-0-0-0
-  cover1_path_2=$gcc12O3_2/checkpoint-0-0-0/cluster-0-0.json
-  cover3_path_2=$PERF_HOME/json/gcc12o3-incFpcOff-jeMalloc-0.3.json
-  cover8_path_2=$PERF_HOME/json/gcc12o3-incFpcOff-jeMalloc-0.8.json
-}
-set_env
+	  gcc12O3_2=/nfs/home/share/liyanqin/spec06_rv64gcb_O3_20m_gcc12.2.0-intFpcOff-jeMalloc
+	  cpt_path_2=$gcc12O3_2/checkpoint-0-0-0
+	  cover1_path_2=$gcc12O3_2/checkpoint-0-0-0/cluster-0-0.json
+	  cover3_path_2=$PERF_HOME/json/gcc12o3-incFpcOff-jeMalloc-0.3.json
+	  cover8_path_2=$PERF_HOME/json/gcc12o3-incFpcOff-jeMalloc-0.8.json
+	}
+	set_env
 
-########## param: YOU SHOULD CONFIRM ##########
-if [[ $(hostname) == *"open"* ]]; then
-## small server
-  server_list="open07 open08 open09 open10 open12 open13 open14 open15" #open23 open24 open25 open26 open27"
-elif [[ $(hostname) == *"node"* ]]; then
-## big server
-  server_list="node020 node021 node022 node023 node024 node025 node026 node027 node028 node029 node036 node037 node038 node039 node040" #node003 node004"
-else
-  echo "unsupported hostname: $(hostname)"
-  exit 1
-fi
+	########## param: YOU SHOULD CONFIRM ##########
+	if [[ $(hostname) == *"open"* ]]; then
+	## small server
+	  server_list="open07 open08 open09 open10 open12 open13 open14 open15" #open23 open24 open25 open26 open27"
+	elif [[ $(hostname) == *"node"* ]]; then
+	## big server
+	  server_list="node020 node021 node022 node023 node024 node025 node026 node027 node028 node029 node036 node037 node038 node039 node040" #node003 node004"
+	else
+	  echo "unsupported hostname: $(hostname)"
+	  exit 1
+	fi
 
-echo $server_list
+	echo $server_list
 
-cpt_path=$cpt_path_2
-json_path=$cover3_path_2
-threads=16 # notice threads should keep same between build.sh and run.sh
-version="kunminghu"
+	cpt_path=$cpt_path_2
+	json_path=$cover3_path_2
+	threads=16 # notice threads should keep same between build.sh and run.sh
+	version="kunminghu"
 
-if [[ $1 ]]; then
-  spec_dir=$1
-else
-  spec_dir="SPEC06_EmuTasks_"$(date +%m%d_%H%M)
-fi
+	if [[ $1 ]]; then
+	  spec_dir=$1
+	else
+	  spec_dir="SPEC06_EmuTasks_"$(date +%m%d_%H%M)
+	fi
 
-[ -e server_bad ] && rm server_bad
-echo "Checking servers..."
-for server in $server_list; do
-  ssh -o ConnectTimeout=20 -o BatchMode=yes "$server" "exit" &>/dev/null && echo "OK: $server" || { echo "FAIL: $server"; touch server_bad; } &
-done
-wait
+	[ -e server_bad ] && rm server_bad
+	echo "Checking servers..."
+	for server in $server_list; do
+	  ssh -o ConnectTimeout=20 -o BatchMode=yes "$server" "exit" &>/dev/null && echo "OK: $server" || { echo "FAIL: $server"; touch server_bad; } &
+	done
+	wait
 
-[ -e server_bad ] && { echo "server bad"; rm server_bad; exit 1; } || echo "server good"
+	[ -e server_bad ] && { echo "server bad"; rm server_bad; exit 1; } || echo "server good"
 
-echo "Done"
+	echo "Done"
 
-########## run ##########
-echo "********** cal start at $(date) **********"
-echo "checkpoint test: $spec_dir"
-cd $HOME/env-scripts/perf
-python3 xs_autorun_multiServer.py $cpt_path $json_path --xs $NOOP_HOME --threads $threads --dir $spec_dir --resume -L "$server_list" 2>&1 | tee $pwd_tmp/.run.log
-echo "********** cal end at $(date) **********"
-
+  ########## run ##########
+  echo "********** cal start at $(date) **********"
+  echo "checkpoint test: $spec_dir"
+  cd $HOME/env-scripts/perf
+  python3 xs_autorun_multiServer.py $cpt_path $json_path --xs $NOOP_HOME --threads $threads --dir $spec_dir --resume -L "$server_list" 2>&1 | tee $pwd_tmp/.run.log
+  echo "********** cal end at $(date) **********"
+)}
+func
 python3 /nfs/home/share/liyanqin/scripts/ShareAutoEmailAlert.py -r $? --content "running spec test at dir: $spec_dir finish"
